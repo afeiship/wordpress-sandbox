7051a6483dcaa7c4d5791305d7f18e6cc31dff73
feizheng <1290657123@qq.com>
2016-11-13 13:04:55 +0800

    add baidu push

--- src/footer.php
+++ src/footer.php
@@ -1,3 +1,9 @@
 <footer>
   Footer!!!
 </footer>
+
+
+
+<?php
+  require get_template_directory() . '/inc/baidu_js_push.php';
+?>
---
+++ src/inc/baidu_js_push.php
@@ -0,0 +1,16 @@
+<!-- baidu_js_push start 2016年11月13 11时58分37秒-->
+<script>
+(function(){
+    var bp = document.createElement('script');
+    var curProtocol = window.location.protocol.split(':')[0];
+    if (curProtocol === 'https') {
+        bp.src = 'https://zz.bdstatic.com/linksubmit/push.js';
+    }
+    else {
+        bp.src = 'http://push.zhanzhang.baidu.com/push.js';
+    }
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(bp, s);
+})();
+</script>
+<!-- baidu_js_push end 2016年11月13 11时58分37秒 -->
